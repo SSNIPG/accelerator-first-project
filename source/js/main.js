4353e7626cf67608cfe8d0d4da33da7f480e315e
Сергей Сакович <imperator42@rambler.ru>
2024-04-22 17:42:38 +0300

    Добавляет разметку и стилизацию блока tours

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,64 @@
 // https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
-import { onButtonClick } from '/js/menu-toggle/menu-toggle.js';
+import { onButtonClick } from './modules/menu-toggle/menu-toggle.js';
+// import { getSlider } from './modules/slider/slider.js';
+
 onButtonClick();
+// getSlider();
+
+// import Swiper from 'swiper';
+// import 'swiper/css';
+// import { Pagination } from 'swiper/modules';
+// import 'swiper/css/pagination';
+// const heroSwiper = document.querySelector('.hero__swiper');
+
+// const heroSlider = new Swiper(heroSwiper, {
+
+//   modules: [Pagination],
+//   direction: 'horizontal',
+//   loop: true,
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1,
+//       initialSlide: 2,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//       spaceBetween: 40,
+//       initialSlide: 0,
+//     },
+//     1366: {
+//       slidesPerView: 4,
+//       spaceBetween: 40,
+//     }
+//   },
+// });
+
+// export { heroSlider };
+
+// const getSlider = () => {
+//   let heroSlider;
+
+//   if (heroSwiper) {
+//     heroSlider = new Swiper(heroSwiper, {
+//       modules: [Pagination],
+
+//       direction: 'horizontal',
+//       loop: false,
+//       speed: 1000,
+//       // breakpoints: {
+//       //   320: {
+//       //     slidesPerView: 1,
+//       //     spaceBetween: 40,
+//       //   },
+//       // },
+//       // navigation: {
+//       //   nextEl: '.reviews__button--next',
+//       //   prevEl: '.reviews__button--prev',
+//       // },
+//     });
+//   }
+//   return { heroSlider };
+// };
+
+// export { getSlider };
+// getSlider();
