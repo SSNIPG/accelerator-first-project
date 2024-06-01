e7816f3b512722f6d976c87f660fa6750464fb04
Сергей Сакович <imperator42@rambler.ru>
2024-06-01 05:08:17 +0300

    Добавляет разметку и стилизацию блока programs

--- source/js/modules/slider/init-slider.js
+++ source/js/modules/slider/init-slider.js
@@ -1,10 +1,16 @@
 import Swiper from 'swiper';
-import { Pagination } from 'swiper/modules';
+import { Pagination, Navigation, Scrollbar } from 'swiper/modules';
 import 'swiper/css';
-const promoSwiper = document.querySelector('.promo__swiper');
+const promo = document.querySelector('.promo');
+const promoSwiper = promo.querySelector('.promo__swiper');
+const programs = document.querySelector('.programs');
+const programsSwiper = programs.querySelector('.programs__swiper');
+const programsBtnNext = programs.querySelector('.programs__button--next');
+const programsBtnPrev = programs.querySelector('.programs__button--prev');
 
 const getSlider = () => {
   let promoSlider;
+  let programsSlider;
 
   if (promoSwiper) {
     promoSlider = new Swiper(promoSwiper, {
@@ -32,7 +38,40 @@ const getSlider = () => {
   //   pagination.style.bottom = `${ contentHeight }px`;
   //   // pagination.style.left = `${0}px`;
   // });
-  return { promoSlider };
+
+  if (programsSwiper) {
+    programsSlider = new Swiper(programsSwiper, {
+      modules: [Navigation, Pagination, Scrollbar],
+      direction: 'horizontal',
+      loop: false,
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+        768: {
+          // slidesPerView: 2,
+          slidesPerView: 'auto',
+          spaceBetween: 30,
+        },
+        1440: {
+          slidesPerView: 3,
+          // slidesPerView: 'auto',
+          spaceBetween: 32,
+          allowTouchMove: false,
+        },
+      },
+      scrollbar: {
+        el: '.programs__scrollbar',
+        draggable: true,
+      },
+      navigation: {
+        nextEl: programsBtnNext,
+        prevEl: programsBtnPrev,
+      },
+    });
+  }
+  return { promoSlider, programsSlider };
 };
 
 export { getSlider };
