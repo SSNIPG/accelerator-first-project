7e1d7394ad1b21e09650710e9ebbf1347c66b388
Сергей Сакович <imperator42@rambler.ru>
2024-06-03 17:15:51 +0300

    Добавляет разметку и стилизацию блока faq

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,10 @@
 import { getSlider } from './modules/slider/init-slider';
 import { onButtonClick, onLinkClick, onWindowClick } from './modules/menu-toggle/menu-toggle.js';
+import { onNoInteractiveElementClick, onNoInteractiveElementKeydown } from './modules/faq-buttons/faq-buttons.js';
 
 getSlider();
 onButtonClick();
 onLinkClick();
 onWindowClick();
+onNoInteractiveElementKeydown();
+onNoInteractiveElementClick();
---
+++ source/js/modules/faq-buttons/faq-buttons.js
@@ -0,0 +1,33 @@
+const faq = document.querySelector('.faq');
+const faqItemInners = faq.querySelectorAll('.faq__item-inner');
+const faqListButtons = faq.querySelectorAll('.faq__list-button');
+const faqItem = faq.querySelectorAll('.faq__item');
+
+function onNoInteractiveElementKeydown () {
+  for (let i = 0; i < faqItemInners.length; i++) {
+    faqItemInners[i].addEventListener('keydown', (event) => {
+      if (event.key === 'Enter' || event.key === ' ') {
+        faqItem[i].classList.toggle('faq__item--active');
+        faqListButtons[i].classList.toggle('faq__list-button--active');
+
+      }
+    });
+  }
+}
+
+function onNoInteractiveElementClick() {
+  for (let i = 0; i < faqItemInners.length; i++) {
+    if (faqListButtons[i].disabled) {
+      faqItem[i].classList.toggle('faq__item--disabled');
+      faqItemInners[i].removeAttribute('tabindex');
+    }
+    faqItemInners[i].addEventListener('click', () => {
+      if (!faqListButtons[i].disabled) {
+        faqItem[i].classList.toggle('faq__item--active');
+        faqListButtons[i].classList.toggle('faq__list-button--active');
+      }
+    });
+  }
+}
+
+export { onNoInteractiveElementKeydown, onNoInteractiveElementClick };
